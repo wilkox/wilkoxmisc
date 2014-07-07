4ca21ac2857e71215822cce8041d8b67768c998c
David Wilkins <david@wilkox.org>
2014-07-07 17:13:20 +0800

    Allow splitting Minor/Unclassified in taxa table collapse

--- R/collapse.taxa.table.R
+++ R/collapse.taxa.table.R
@@ -1,39 +1,39 @@
-collapse.taxa.table <- function (TaxaTable, Rank = "Phylum", n = 7, RelativeAbundance = "RelativeAbundance", CollapseName = "Minor/Unclassified", Groups = c("Sample"), Omit = TRUE) {
-
-  #Rename Rank and RelativeAbundance columns for convenience
-  names(TaxaTable)[names(TaxaTable) == RelativeAbundance] <- "RelativeAbundance"
-  names(TaxaTable)[names(TaxaTable) == Rank] <- "Rank"
+collapse.taxa.table <- function (TaxaTable, Rank = "Phylum", n = 8, Groups = c("Sample"), MergeMinorUnclassified = TRUE) {
 
   #Collapse relative abundance by taxon and group
-  TaxaTable <- ddply(TaxaTable, c(Groups, "Rank"), summarise, RelativeAbundance = sum(RelativeAbundance))
+  TaxaTable <- ddply(TaxaTable, c(Groups, Rank), summarise, RelativeAbundance = sum(RelativeAbundance))
 
   #Summarise taxa by relative abundance and sort
-  TopTaxa <- ddply(TaxaTable, .(Rank), summarise, RelativeAbundance = mean(RelativeAbundance))
+  TopTaxa <- ddply(TaxaTable, c(Rank), summarise, RelativeAbundance = mean(RelativeAbundance))
   TopTaxa <- arrange(TopTaxa, desc(RelativeAbundance))
 
-  #Omit na/'' taxa
-  if (Omit) {
-    TopTaxa <- na.omit(TopTaxa)
-    TopTaxa <- TopTaxa[which(TopTaxa["Rank"] != ""), ]
+  #Omit blank taxa
+  TopTaxa <- TopTaxa[which(TopTaxa[Rank] != ""), ]
+
+  #Select top taxa
+  TopTaxa <- as.character(TopTaxa[[Rank]])
+  TopTaxa <- TopTaxa[1:ifelse(MergeMinorUnclassified, n - 1, n - 2)]
+
+  #Replace unclassified with "Unclassified", if requested
+  if (! MergeMinorUnclassified) {
+    TaxaTable[[Rank]] <- ifelse(TaxaTable[[Rank]] == "", "Unclassified", as.character(TaxaTable[[Rank]]))
+    TopTaxa <- c(TopTaxa, "Unclassified")
   }
 
-  #Select top n taxa
-  TopTaxa <- TopTaxa[1:n, ]
-  TopTaxa <- TopTaxa$Rank
-
-  #Replace non-top taxa with the collapse name
-  TaxaTable <- within(TaxaTable, Rank <- ifelse(Rank %in% TopTaxa, as.character(Rank), CollapseName))
+  #Replace minor taxa with the minor name
+  TaxaTable[[Rank]] <- ifelse(TaxaTable[[Rank]] %in% TopTaxa, as.character(TaxaTable[[Rank]]), ifelse(MergeMinorUnclassified, "Minor/Unclassified", "Minor"))
 
   #Collapse the table
-  TaxaTable <- ddply(TaxaTable, c(Groups, "Rank"), summarise, RelativeAbundance = sum(RelativeAbundance))
+  TaxaTable <- ddply(TaxaTable, c(Groups, Rank), summarise, RelativeAbundance = sum(RelativeAbundance))
 
-  #Refactorise the taxon and move collapse name to end
-  TaxaTable$Rank <- factor(TaxaTable$Rank)
-  TaxaTable$Rank <- move.to.end(TaxaTable$Rank, CollapseName)
-
-  #Restore original taxon and relative abundance names
-  names(TaxaTable)[names(TaxaTable) == "RelativeAbundance"] <- RelativeAbundance
-  names(TaxaTable)[names(TaxaTable) == "Rank"] <- Rank
+  #Refactorise the taxon and move collapse name(s) to end
+  TaxaTable[Rank] <- factor(TaxaTable[[Rank]])
+  if (MergeMinorUnclassified) {
+    TaxaTable[[Rank]] <- move.to.end(TaxaTable[[Rank]], "Minor/Unclassified")
+  } else {
+    TaxaTable[[Rank]] <- move.to.end(TaxaTable[[Rank]], "Minor")
+    TaxaTable[[Rank]] <- move.to.end(TaxaTable[[Rank]], "Unclassified")
+  }
 
   return(TaxaTable)
 }
