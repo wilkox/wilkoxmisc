b1bfed06088566f3e7bc1bc04a8fda607a7b3b81
David Wilkins <david@wilkox.org>
2014-07-14 15:28:55 +0800

    Improve add.relative.abundance efficiency

--- R/add.relative.abundance.R
+++ R/add.relative.abundance.R
@@ -1,4 +1,3 @@
 add.relative.abundance <- function(OTUTable) {
-  OTUTable <- ddply(OTUTable, .(Sample), mutate, RelativeAbundance = (100 * Count) / sum(Count), .progress = 'time')
-  return(OTUTable)
+  return(ddply(OTUTable, .(Sample), mutate, RelativeAbundance = (100 * Count) / sum(Count), .progress = 'time'))
 }
