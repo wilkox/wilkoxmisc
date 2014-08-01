d46bea18fcc02370c3e63338865a0961138be7c1
David Wilkins <david@wilkox.org>
2014-08-01 13:01:17 +0800

    Add matrix support to write.tidy

--- R/tidy_files.R
+++ R/tidy_files.R
@@ -1,15 +1,19 @@
 read.tidy <- function (fileName, ...) {
-  df <- read.csv(fileName, head = TRUE, row.names = NULL, sep = "\t", 
-  check.names = FALSE, ...)
-  return(df)
+    df <- read.csv(fileName, head = TRUE, row.names = NULL, sep = "\t", check.names = FALSE, ...)
+    return(df)
 }
 
-write.tidy <- function (dataFrame, fileName) {
-  write.table(dataFrame, fileName, quote = FALSE, sep = "\t", 
-  row.names = FALSE)
+write.tidy <- function (data, fileName) {
+
+    if (is.matrix(data)) {
+        write.table(data, fileName, quote = FALSE, col.names = NA, row.names = TRUE, sep = "\t")
+
+    } else {
+        write.table(data, fileName, quote = FALSE, sep = "\t", row.names = FALSE)
+    }
 }
 
 read.tidy.dt <- function(Path) {
-  DataTable <- data.table(read.tidy(Path))
-  return(DataTable)
+    DataTable <- data.table(read.tidy(Path))
+    return(DataTable)
 }
