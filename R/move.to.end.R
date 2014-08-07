cace4d4a8287e70c3fd47b7395897e93cf64ee96
David Wilkins <david@wilkox.org>
2014-08-07 13:23:16 +0800

    Document move.to.end

--- R/move.to.end.R
+++ R/move.to.end.R
@@ -1,5 +1,13 @@
-move.to.end <-
-function (Factor, Level) 
+#' @title Move a factor level to the end of the factor
+#'
+#' @description
+#'
+#' Move a factor level to the end of the factor.
+#' Useful when plotting.
+#'
+#' @param Factor the factor
+#' @param Level the level to move to the end
+move.to.end <- function (Factor, Level) 
 {
     Levels <- levels(factor(Factor))
     Levels <- Levels[Levels != Level]
