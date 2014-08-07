a4e84bbded821a100734bf32170d8b6ab0f8759d
David Wilkins <david@wilkox.org>
2014-08-07 12:06:09 +0800

    Trial add documentation

--- R/tidy_files.R
+++ R/tidy_files.R
@@ -1,15 +1,21 @@
-read.tidy <- function (fileName, ...) {
-    df <- read.csv(fileName, head = TRUE, row.names = NULL, sep = "\t", check.names = FALSE, ...)
+#' Load a data frame from a file in ‘tidy’ format.
+#'
+#' See http://vita.had.co.nz/papers/tidy-data.pdf for more info on ‘tidy data’.
+#'
+#' @param file the name of the file data will be read from
+#' @param ... further arguments to be passed to ‘read.csv’
+read.tidy <- function (file, ...) {
+    df <- read.csv(file, head = TRUE, row.names = NULL, sep = "\t", check.names = FALSE, ...)
     return(df)
 }
 
-write.tidy <- function (data, fileName) {
+write.tidy <- function (data, file) {
 
     if (is.matrix(data)) {
-        write.table(data, fileName, quote = FALSE, col.names = NA, row.names = TRUE, sep = "\t")
+        write.table(data, file, quote = FALSE, col.names = NA, row.names = TRUE, sep = "\t")
 
     } else {
-        write.table(data, fileName, quote = FALSE, sep = "\t", row.names = FALSE)
+        write.table(data, file, quote = FALSE, sep = "\t", row.names = FALSE)
     }
 }
 
