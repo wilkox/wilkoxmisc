7909d092c31de3076fef515a45b55201894cd169
David Wilkins <david@wilkox.org>
2014-08-07 13:13:40 +0800

    Document read.dist and write.dist

--- R/dist_files.R
+++ R/dist_files.R
@@ -1,13 +1,24 @@
-read.dist <-
-function (Filename) 
+#' @title Read and write dist objects to file
+#' @rdname dist_files
+#' @family dist_files
+#'
+#' @description
+#' Read or write an object of type "dist" from/to file.
+#'
+#' @param file path for reading or writing.
+#' @param dist object of type "dist" to write.
+#' @param ... further arguments to pass to "read.csv" or "write.table"
+#'
+#' @seealso dist
+read.dist <- function (file, ...) 
 {
-    return(as.dist(read.csv(Filename, sep = "\t", row.names = 1)))
+    return(as.dist(read.csv(file, sep = "\t", row.names = 1, ...)))
 }
 
-write.dist <-
-function (Dist, Filename) 
-{
-    Matrix <- as.matrix(Dist)
-    write.table(Matrix, Filename, quote = FALSE, col.names = NA, 
-        row.names = TRUE, sep = "\t")
+#' @rdname dist_files
+#' @family dist_files
+write.dist <- function (dist, file, ...) {
+    Matrix <- as.matrix(dist)
+    write.table(Matrix, file, quote = FALSE, col.names = NA, 
+        row.names = TRUE, sep = "\t", ...)
 }
