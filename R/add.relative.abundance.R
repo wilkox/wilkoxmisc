e99b451f1d809c3c553edf4c6839ad764e8bcf5e
David Wilkins <david@wilkox.org>
2014-08-07 12:33:58 +0800

    Document add.relative.abundance

--- R/add.relative.abundance.R
+++ R/add.relative.abundance.R
@@ -1,3 +1,10 @@
+#' @title Add relative abundance to an OTU table
+#'
+#' @description
+#' Takes an OTU table in tidy format, i.e. with at least "Sample", "OTU" and "Count" columns.
+#' Appends a "RelativeAbundance" column containing the relative abundance (expressed as a percentage) of each OTU within each sample.
+#'
+#' @param OTUTable the OTU table, which must be a tidy data frame with at least "Sample", "OTU" and "Count" columns
 add.relative.abundance <- function(OTUTable) {
   return(ddply(OTUTable, .(Sample), mutate, RelativeAbundance = (100 * Count) / sum(Count), .progress = 'time'))
 }
