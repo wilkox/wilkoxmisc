c812f27c40f29998ec48215d89124950c714d7d6
David Wilkins <david@wilkox.org>
2014-08-20 10:02:01 +0800

    Add Greengenes taxonomy

--- R/data.R
+++ R/data.R
@@ -6,3 +6,12 @@
 #' @description
 #' Data frame of indicator families for various sources.
 NULL
+
+#' @title GreengenesTaxonomy
+#' @name GreengenesTaxonomy
+#' @docType data
+#' @references McDonald, D., Price, M. N., Goodrich, J., Nawrocki, E. P., DeSantis, T. Z., Probst, A., et al. (2011). An improved Greengenes taxonomy with explicit ranks for ecological and evolutionary analyses of bacteria and archaea. The ISME Journal, 6(3), 610-618. doi:10.1038/ismej.2011.139
+#' @keywords data
+#' @description
+#' A data frame of all lineages in the Greengenes taxonomy.
+NULL
