ec1b7177bae4f3137cfc8126d812d456b8923de0
David Wilkins <david@wilkox.org>
2014-09-19 11:19:12 +0800

    Improvements to add.lineage.description and deepest.rank

--- R/add.lineage.description.R
+++ R/add.lineage.description.R
@@ -2,41 +2,50 @@
 #' @export
 #'
 #' @description
-#' Takes a named vector (assumed to be a row from a data frame with columns "OTU", "Kingdom", "Phylum", "Class" etc.) and constructs a human-readable lineage description.
+#' Takes a data frame with columns "OTU", "Kingdom", "Phylum", "Class" etc. and constructs a human-readable lineage description.
 #' It will try to use the binomial if possible.
-#' Returns the input vector, with an additional "Description" element.
+#' Returns the data frame, with an additional "Description" column.
 #'
-#' @param OTU named vector
-#' @param add_OTU add the OTU name in brackets to the end of the description.
-add.lineage.description <- function(OTU, add_OTU = TRUE) {
+#' @param OTUTable data frame with at least an "OTU" column and at least one taxonomic rank column
+#' @param add_OTU add the OTU name to the description
+#' @param italicise_binomials if set to "markdown" or "md", will wrap binomials in asterisks so they appear italicised in markdown output
+add.lineage.description <- function(OTUTable, add_OTU = TRUE, italicise_binomials = NULL) {
+
+  #Operate row-by-row
+  if (nrow(OTUTable) > 1) {
+    return(rbind(add.lineage.description(OTUTable[1,]), add.lineage.description(OTUTable[2:nrow(OTUTable), ])))  
+  }
 
   #If species exists, return binomial
-  if (! is.blank(OTU["Species"])) {
-    OTU$Description <- paste(as.character(OTU$Genus), as.character(OTU$Species))
-      if (add_OTU) {
-        OTU$Description <- paste0(OTU$Description, " (", OTU$OTU, ")")
-      }
-    return(OTU)
+  if (! is.blank(OTUTable["Species"])) {
+    OTUTable$Description <- paste(as.character(OTUTable$Genus), as.character(OTUTable$Species))
+    if (italicise_binomials == "markdown" | italicise_binomials == "md") {
+      OTUTable$Description <- paste0("*", OTUTable$Description, "*")
+    }
+    if (add_OTU) {
+      OTUTable$Description <- paste0(OTUTable$Description, " (", OTUTable$OTU, ")")
+    }
+    return(OTUTable)
   }
 
   #Identify the deepest rank for which there is taxonomic information
-  DeepestRank <- deepest.rank(OTU)
+  DeepestRank <- deepest.rank(OTUTable)
 
   #Return deepest rank
-  OTU$Description <- paste0(DeepestRank, ": ", OTU[[DeepestRank]])
+  OTUTable$Description <- paste0(DeepestRank, ": ", OTUTable[[DeepestRank]])
   if (add_OTU) {
-    OTU$Description <- paste0(OTU$Description, " (", OTU$OTU, ")")
+    OTUTable$Description <- paste0(OTUTable$Description, " (", OTUTable$OTU, ")")
   }
-  return(OTU)
+  return(OTUTable)
 }
 
 #' @title Identify the deepest rank for which there is taxonomic information
 #' @export
 #'
 #' @description
-#' Takes a named vector (assumed to be a row from a data frame with columns "OTU", "Kingdom", "Phylum", "Class" etc.) and returns the name of the deepest rank with taxonomic information.
+#' Takes a row from a data frame with columns "OTU" and at least one taxonomic rank, and returns the name of the deepest rank with taxonomic information.
 #'
-#' @param OTU named vector
+#' @param OTU row from a data frame with columns "OTU" and at least one taxonomic rank
 #' @param Ranks vector with names of ranks to look for, in order
 #'
 deepest.rank <- function(OTU, Ranks = c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species")) {
