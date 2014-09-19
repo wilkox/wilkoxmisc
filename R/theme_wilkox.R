3bb915f2e70258462f51364de74b8f72f684cdd4
David Wilkins <david@wilkox.org>
2014-09-19 13:11:36 +0800

    Add theme_wilkox

---
+++ R/theme_wilkox.R
@@ -0,0 +1,21 @@
+#' @title Standardised ggplot2 theme for publications
+#' @export
+#'
+#' @usage Plot + theme_wilkox()
+theme_wilkox <- function() {
+  theme_classic(base_size = 8, base_family = "Helvetica") %+replace% theme(
+    panel.grid = element_blank(),
+    axis.line = element_line(lineend = "square"),
+    axis.ticks.length = unit(1, "mm"),
+    axis.title.x = element_text(vjust = 0),
+    axis.title.y = element_text(vjust = 1, angle = 90),
+    axis.text = element_text(size = 6),
+    legend.background = element_blank(),
+    legend.key = element_blank(),
+    legend.position = "bottom",
+    legend.title = element_blank(),
+    legend.margin = unit(-6, "mm"),
+    strip.background = element_blank(),
+    strip.text = element_text(face = "italic")
+  )
+}
