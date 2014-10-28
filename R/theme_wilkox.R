708a1d98e180f7fce123a6ec86c753f14bd394f0
David Wilkins <david@wilkox.org>
2014-10-28 10:22:37 +0800

    Restore Helvetica to theme_wilkox

--- R/theme_wilkox.R
+++ R/theme_wilkox.R
@@ -4,27 +4,20 @@
 #' @usage Plot + theme_wilkox()
 #' Plot + theme_wilkox() \%+replace\% theme(...)
 theme_wilkox <- function() {
-
-  # Check if DIN fonts are available
-  DIN <- "DIN" %in% names(pdfFonts()) & "DINCond-Regular" %in% names(pdfFonts()) & "DINPro-Bold" %in% names(pdfFonts())
-  RegularFont <- ifelse(DIN, "DIN", "Helvetica")
-  CondensedFont <- ifelse(DIN, "DINCond-Regular", "Helvetica-Narrow")
-  BoldFont <- ifelse(DIN, "DINPro-Bold", "Helvetica")
-
-  theme_bw(base_size = 8, base_family = RegularFont) %+replace% theme(
+  theme_bw(base_size = 8, base_family = "Helvetica") %+replace% theme(
     panel.grid = element_blank(),
     panel.border = element_blank(),
     axis.line = element_line(lineend = "square"),
     axis.ticks.length = unit(1, "mm"),
-    axis.title.x = element_text(vjust = 0, size = 9, family = RegularFont),
-    axis.title.y = element_text(vjust = 1, angle = 90, size = 9, family = RegularFont),
-    axis.text = element_text(size = 8, family = CondensedFont),
+    axis.title.x = element_text(vjust = 0, size = 10),
+    axis.title.y = element_text(vjust = 1, angle = 90, size = 10),
+    axis.text = element_text(size = 8, family = "Helvetica-Narrow", face = "bold"),
     legend.background = element_blank(),
     legend.key = element_blank(),
-    legend.text = element_text(lineheight = 2, size = 10, family = CondensedFont),
-    legend.title = element_text(lineheight = 4, size = 8, family = BoldFont),
+    legend.text = element_text(lineheight = 4, size = 8, family = "Helvetica-Narrow", face = "bold"),
+    legend.title = element_text(lineheight = 4, size = 8, family = "Helvetica", face = "bold"),
     legend.margin = unit(-6, "mm"),
     strip.background = element_blank(),
-    strip.text = element_text(size = 8, family = BoldFont)
+    strip.text = element_text(face = "italic")
   )
 }
