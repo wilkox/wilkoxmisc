bbe77bb81b389a29202497fd403904de8282d1c2
David Wilkins <david@wilkox.org>
2015-05-20 11:38:03 +1000

    Tweak write.pdf

--- R/write.pdf.R
+++ R/write.pdf.R
@@ -3,19 +3,19 @@
 #'
 #' @description
 #' Write a data frame to pdf using kable (from knitr) and pandoc. Pandoc
-#' must be installed.
+#' must be installed. xelatex is used as the latex engine.
 #'
 #' @param Data dataframe to write
+#' @param File file to write to (must end in .pdf)
 #' @param ... further arguments to pass to kable
-write.pdf <- function(Data, ...) {
+write.pdf <- function(Data, File, ...) {
 
   if(! is.data.frame(Data)) {
     return()
   }
   
-  Literal <- deparse(substitute(Data))
   Markdown <- kable(Data, format = "markdown", ...)
   write(Markdown, "/tmp/dataframe.md")
-  system(paste0("pandoc /tmp/dataframe.md -o ", Literal, ".pdf"))
+  system(paste0("pandoc /tmp/dataframe.md -o ", File, " --latex-engine=xelatex"))
   unlink("/tmp/dataframe.md")
 }
