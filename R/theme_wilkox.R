4f0c10bfb3342bd9310f135c4570d066728e9729
David Wilkins <david@wilkox.org>
2016-01-02 18:57:24 +1100

    Tweak theme_wilkox

--- R/theme_wilkox.R
+++ R/theme_wilkox.R
@@ -4,17 +4,16 @@
 #' @usage Plot + theme_wilkox()
 #' Plot + theme_wilkox() \%+replace\% theme(...)
 theme_wilkox <- function() {
-  theme_bw(base_size = 8, base_family = "Helvetica") %+replace% theme(
+  theme_classic(base_size = 10, base_family = "Helvetica") %+replace% theme(
     panel.grid = element_blank(),
     panel.border = element_blank(),
     axis.line = element_line(lineend = "square"),
     axis.ticks.length = unit(0.5, "mm"),
-    axis.title.x = element_text(vjust = 0, size = 10),
-    axis.title.y = element_text(vjust = 1, angle = 90, size = 10),
+    axis.title = element_text(size = 12),
     axis.text = element_text(
-      size = 5,
+      size = 8,
       family = "Helvetica",
-      face = "bold.italic"
+      lineheight = 10
     ),
     legend.background = element_blank(),
     legend.key = element_blank(),
@@ -30,7 +29,7 @@ theme_wilkox <- function() {
       face = "bold"
     ),
     legend.margin = unit(-6, "mm"),
-    strip.background = element_blank(),
-    strip.text = element_text(face = "italic")
+    strip.text = element_text(face = "italic"),
+    strip.background = element_blank()
   )
 }
