acf350ca2f0a97492110710ff99e055d66b08376
David Wilkins <david@wilkox.org>
2016-09-27 11:39:27 +1000

    v0.3: Deprecate write_tidy for readr::write_tsv

--- R/write_tidy.R
+++ R/write_tidy.R
@@ -1,21 +1,16 @@
-#' @title Write a tbl to tsv
+#' @title (DEPRECATED) Write a tbl to tsv
 #' @rdname write_tidy
 #' @export
 #'
 #' @description
-#' 'write_tidy' writes a tbl to file in tidy format. It's just a convenience
-#' wrapper for 'write.table' with arguments set for tidy-formatted files. 
+#' write_tidy() is deprecated; use readr::write_tsv() instead
 #'
-#' @param file path of the file data will be read from
-#' @param data tbl to be written
-#' @param ... further arguments to be passed to 'write.table'
-#'
-#' @seealso readr::read_tsv
+#' @seealso readr::write_tsv
 #' @rdname tidy_files
 #' @family tidy_files
 #' @export
-write_tidy <- function (data, file, ...) {
-  write.table(data, file, quote = FALSE, sep = "\t", row.names = FALSE, ...)
+write_tidy <- function (...) {
+  warning("write_tidy() is deprecated; use readr::write_tsv() instead")
 }
 
 #' @title (DEPRECATED) Read a data frame from tsv
@@ -35,7 +30,7 @@ read.tidy <- function(...) {
 #' @export
 #'
 #' @description 
-#' write.tidy() is deprecated; use write_tidy() instead
+#' write.tidy() is deprecated; use readr::write_tsv() instead
 write.tidy <- function(...) {
-  warning("write.tidy() is deprecated; use write_tidy() instead")
+  warning("write.tidy() is deprecated; use readr::write_tsv() instead")
 }
