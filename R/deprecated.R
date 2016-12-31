1cb7e9e285df4c526eae6af01dfab1299976b96a
David Wilkins <david@wilkox.org>
2016-12-31 11:35:41 +1100

    Tidy deprecated functions, deprecate move_to_end

--- R/deprecated.R
+++ R/deprecated.R
@@ -4,20 +4,19 @@
 #' @export
 #'
 #' @description
-#' This function is deprecated. Use the `dplyr` package and the following idiom
+#' `add_relative_abundance` is deprecated. Use the `dplyr` package and the following idiom
 #' instead:
 #'
-#' ```
-#' library(dplyr)
-#' OTUTable %>%
-#'  group_by(Sample) %>%
-#'  mutate(RelativeAbundance = (100 * Count) / sum(Count) %>%
-#'  ungroup()
-#' 
+#' \preformatted{library(dplyr)
+#' OTUTable \%>\%
+#'  group_by(Sample) \%>\%
+#'  mutate(RelativeAbundance = (100 * Count) / sum(Count) \%>\%
+#'  ungroup}
+#'
 #' @param OTUTable the OTU table in a tbl, with at least 'Sample', 'OTU' and
 #' 'Count' columns
 add_relative_abundance <- function(...) {
-  stop("This function is deprecated. See ?add_relative_abundance for details.", call. = FALSE)
+  stop("`add_relative_abundance` is deprecated. See ?add_relative_abundance for details.", call. = FALSE)
 }
 
 #' @title  (DEPRECATED) Add relative abundance to an OTU table
@@ -33,14 +32,14 @@ add.relative.abundance <- function(...) {
 #' @export
 #'
 #' @description
-#' write_tidy() is deprecated; use readr::write_tsv() instead
+#' `write_tidy` is deprecated; use `readr::write_tsv` instead
 #'
 #' @seealso readr::write_tsv
 #' @rdname tidy_files
 #' @family tidy_files
 #' @export
 write_tidy <- function (...) {
-  stop("write_tidy() is deprecated; use readr::write_tsv() instead", call. = FALSE)
+  stop("`write_tidy` is deprecated; use readr::`write_tsv` instead", call. = FALSE)
 }
 
 #' @title (DEPRECATED) Read a data frame from tsv
@@ -49,9 +48,9 @@ write_tidy <- function (...) {
 #' @export
 #'
 #' @description 
-#' read.tidy() is deprecated; use readr::read_tsv() instead
+#' `read.tidy` is deprecated; use `readr::read_tsv` instead
 read.tidy <- function(...) {
-  stop("read.tidy() is deprecated; use readr::read_tsv() instead", call. = FALSE)
+  stop("`read.tidy` is deprecated; use `readr::read_tsv` instead", call. = FALSE)
 }
 
 #' @title (DEPRECATED) Write a data frame to tsv
@@ -60,17 +59,35 @@ read.tidy <- function(...) {
 #' @export
 #'
 #' @description 
-#' write.tidy() is deprecated; use readr::write_tsv() instead
+#' `write.tidy` is deprecated; use `readr::write_tsv` instead
 write.tidy <- function(...) {
-  stop("write.tidy() is deprecated; use readr::write_tsv() instead", call. = FALSE)
+  stop("`write.tidy` is deprecated; use `readr::write_tsv` instead", call. = FALSE)
 }
 
 #' @title (DEPRECATED) Ensure a tidy data frame is fully crossed
 #' @export
 #'
 #' @description
-#' This function is deprecated; use expand() from the tidyr package instead.
+#' `ensure.fully.crossed` is deprecated; use `tidyr::expand` instead.
 #'
 ensure.fully.crossed <- function(...) {
-  stop("This function is deprecated; use tidyr::expand() instead.", call. = FALSE)
+  stop("`ensure_fully_crossed` is deprecated; use 1tidyr::expand1 instead.", call. = FALSE)
+}
+
+#' @title (DEPRECATED) Move a factor level to the end of the factor
+#' @export
+#'
+#' @description
+#'
+#' `move_to_end` is deprecated in favour of `forcats::fct_relevel`. `forcats`
+#' can be installed as part of the `tidyverse`:
+#'
+#' \code{install.packages("tidyverse")}
+#'
+#' To use `fct_relevel` in a manner similar to `move_to_end`, take advantage of
+#' \code{after = Inf}, which will move the specified factor to the end:
+#' \preformatted{library(forcats)
+#' fct_relevel(Phyla, "Minor/Unclassified", after = Inf)}
+move_to_end <- function(...) {
+  stop("`move_to_end` is deprecated. See ?move_to_end for details.", call. = FALSE)
 }
