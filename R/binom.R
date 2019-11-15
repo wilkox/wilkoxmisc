39064caa79e2b7f26f3a07629b5ec520edd72cf7
David Wilkins <david@wilkox.org>
2019-11-15 13:12:09 +1100

    Add binom()

---
+++ R/binom.R
@@ -0,0 +1,21 @@
+#' @title Format a taxonomic string for use with ggtext
+#' @export
+#'
+#' @param x The string to format
+binom <- function(x) {
+
+  x <- stringr::str_split(x, "[_\\s]")
+
+  x <- purrr:::map_chr(x, ~ dplyr::case_when( 
+      stringr::str_detect(.x, "^sp\\.?$") ~ "sp.",    # either "sp" or "sp." not italicised
+      stringr::str_detect(.x, "[a-z]{2}") ~ .x,       # type/strain not italicised
+      stringr::str_detect(.x, "^[a-z0-9]+$") ~ .x,    # type/strain not italicised
+      stringr::str_detect(.x, "^unclassified$") ~ .x, # unclassified not italicised
+      true ~ stringr::str_c("*", .x, "*")             # anything else italicised
+    ) %>%
+    str_c(collapse = " ") %>%
+    str_replace_all("\\* \\*", " ")
+  )
+
+  return(x)
+}
