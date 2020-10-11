385569511474ec538d595827a88bf35e7a719bec
David Wilkins <david@wilkox.org>
2020-10-11 15:03:28 +1030

    Special-case handling for Deinococcus-Thermus

--- R/binom.R
+++ R/binom.R
@@ -10,24 +10,27 @@ binom <- function(x) {
 
   x <- purrr:::map_chr(x, function(x) {
 
-      if (x[1] == "Candidatus") {
-        x <- str_c("*Candidatus* '", str_c(x[-1], collapse = " "), "'")
-        return(x)
-      }
+    if (x[1] == "Deinococcus" & x[2] == "Thermus") {
+      return("*Deinococcus-Thermus*")
+    }
 
-      dplyr::case_when( 
-        str_detect(x, "^sp\\.?$") ~ "sp.",   # either "sp" or "sp." not italicised
-        str_detect(x, "[A-Z]{2}") ~ x,       # type/strain not italicised
-        str_detect(x, "^[A-Z0-9]+$") ~ x,    # type/strain not italicised
-        str_detect(x, "^unclassified$") ~ x, # unclassified not italicised
-        str_detect(x, "^[oO]ther$") ~ x,     # other not italicised
-        str_detect(x, "^[nN]oname$") ~ x,    # noname not italicised
-        TRUE ~ str_c("*", x, "*")            # anything else italicised
-      ) %>%
+    if (x[1] == "Candidatus") {
+      x <- str_c("*Candidatus* '", str_c(x[-1], collapse = " "), "'")
+      return(x)
+    }
+
+    dplyr::case_when( 
+      str_detect(x, "^sp\\.?$") ~ "sp.",   # either "sp" or "sp." not italicised
+      str_detect(x, "[A-Z]{2}") ~ x,       # type/strain not italicised
+      str_detect(x, "^[A-Z0-9]+$") ~ x,    # type/strain not italicised
+      str_detect(x, "^unclassified$") ~ x, # unclassified not italicised
+      str_detect(x, "^[oO]ther$") ~ x,     # other not italicised
+      str_detect(x, "^[nN]oname$") ~ x,    # noname not italicised
+      TRUE ~ str_c("*", x, "*")            # anything else italicised
+    ) %>%
       str_c(collapse = " ") %>%
       str_replace_all("\\* \\*", " ")
-    }
-  )
+  })
 
   return(x)
 }
